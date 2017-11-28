910c1036a253316744a8c9dd8c6708c2fdc9ec1a
titaniumbaryum <titouan.baillon@gmail.com>
2017-11-28 21:12:14 +0100

    odometry calibrated in coders_test

--- coders_test/coders_test.ino
+++ coders_test/coders_test.ino
@@ -4,7 +4,7 @@
 #include "DifferentialController.hpp"
 IntervalTimer controlTimer;
 Coders coders(33,34,35,36);
-Odometry odometry(0,0,0,.5,2,5000);
+Odometry odometry(0,0,0,170,65,20000);
 //DifferentialController controller(10,0,0,100,0,0);
 void setup() {
   Serial.begin(250000);
@@ -26,6 +26,6 @@ void controlLoop(){
   //controller.update(odometry.getX(),odometry.getY(),odometry.getA());
   //odometry.move(controller.getLeft(),controller.getRight());
   //odometry.move(1000,1000);
-  Serial.print("Left:");Serial.print(cl);Serial.print(" Right:");Serial.print(cr);Serial.print(" X:");Serial.print(odometry.getX());Serial.print(" Y:");Serial.println(odometry.getY());
+  Serial.print("Left:");Serial.print(cl);Serial.print(" Right:");Serial.print(cr);Serial.print(" X:");Serial.print(odometry.getX());Serial.print(" Y:");Serial.print(odometry.getY());Serial.print(" A:");Serial.println(odometry.getA());
 }
 
